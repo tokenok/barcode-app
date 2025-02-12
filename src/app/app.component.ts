3c0aefb228bb9bf3dbf35f214410baaddab4b6f6
unknown <mrjack.is.minty@gmail.com>
2025-02-11 20:25:31 -0600

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+// src/app/app.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<app-home></app-home>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'barcode-app';
+}
